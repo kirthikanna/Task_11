7debb0727c433e65c20f6c09e284994a1976c600
kirthikanna <150948986+kirthikanna@users.noreply.github.com>
2025-02-03 20:35:52 +0000

    Update Task_11.py

--- Task_11.py
+++ Task_11.py
@@ -8,11 +8,14 @@ driver = webdriver.Chrome(service=service)
 driver.get("https://jqueryui.com/droppable/")
 time.sleep(3)
 print(driver.current_url)
+###first switch to the desired frame before performing any actions#####
 driver.switch_to.frame(driver.find_element(By.CLASS_NAME,"demo-frame"))
+#########Whenever we do mouse actions we use ActionChains class####
 actions = ActionChains(driver)
 drag_element = driver.find_element(By.CSS_SELECTOR,"div[id='draggable']")
 drop_element = driver.find_element(By.CSS_SELECTOR,"div[id='droppable']")
 actions.drag_and_drop(source=drag_element,target=drop_element).perform()
+#####get the screenshot##########
 driver.get_screenshot_as_file("draggable_droppable.png")
 time.sleep(3)
 driver.switch_to.default_content()
